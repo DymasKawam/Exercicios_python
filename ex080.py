f8dd65f9058298596df8beebd7b55da1d37d0ac7
DymasKawam <dymaskauam@gmail.com>
2026-06-17 12:09:52 -0300

    oi

--- ex080.py
+++ ex080.py
@@ -1,5 +1,9 @@
 '''Exercício Python 080: Crie um programa onde o usuário possa digitar cinco valores numéricos e cadastre-os em uma lista, já na posição correta de inserção (sem usar o sort()). No final, mostre a lista ordenada na tela.'''
 lista = []
 for c in range (1,6):
-    lista.append(int(input(f'Digite o {c}º número:')))
-    
+    val = int(input(f'Digite o {c}º número:'))
+    if c == 1:
+        lista.append(val)
+    for i, v in enumerate (lista):
+    if val == lista[-1]:
+        print('adicionado no final da lista')
