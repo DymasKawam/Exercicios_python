250a87d909972fc7b4abbf2d3a51d99e6fcafb0b
DymasKawam <dymaskauam@gmail.com>
2026-08-23 12:23:39 -0300

    Exercício fernando

---
+++ Curso em Video/Aulas Curso em Video/Aula19-Dicionário.py
@@ -0,0 +1,71 @@
+#Dicionários são estruturas de dados semelhantes a tuplas e listas, mas com a diferença de que os elementos são armazenados em pares de chave e valor. 
+
+#exemplo = {'nome': 'Gustavo', 'idade': 22, 'sexo': 'M'}
+
+'''
+print(exemplo['nome'])
+print(exemplo['idade'])
+print(exemplo['sexo']) 
+'''
+#para adicionar valores:
+'''
+exemplo['tamanho'] = 1.78
+
+print(exemplo['tamanho'])
+'''
+
+#para excluir valores
+
+'''
+del exemplo['idade']
+
+print(exemplo)
+'''
+
+filmes = {
+'Titulo': 'StarWars',
+'Ano': 1977,
+'diretor': 'George Lucas',
+}
+
+#para pegar as valores do dicionário filme eu posso usar o :
+'''
+print(filmes.values())
+'''
+
+#para pegar as chaves eu posso usar:
+'''
+print(filmes.keys())
+'''
+
+#para pegar os dois eu posso usar:
+'''
+print(filmes.items())
+'''
+
+'''
+for k, v in filmes.items():
+    print(f'O {k} é {v}')
+'''
+
+#também da para juntar Lista com Dicionários
+#exemplo
+
+'''brasil = []
+estado1 = {'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
+estado2 = {'uf': 'São Paulo', 'sigla': 'SP'}
+brasil.append(estado1)
+brasil.append(estado2)
+print(brasil[1]['sigla'])'''
+
+#============================
+estado = {}
+brasil = []
+for c in range (0,3):
+    estado['uf'] = str(input('Unidade Federativa: ')).strip().title()
+    estado['sigla'] = str(input('sigla do estado: ')).strip().upper()
+    brasil.append(estado.copy())
+
+for i in brasil:
+    for k, v in i.items():
+        print(f'O campo {k} tem valor {v}')
---
+++ Curso em Video/exercício4.py
@@ -0,0 +1,40 @@
+'''
+Ler quatro valores numéricos inteiros e apresentar o resultado dois a dois da adição e multiplicação entre os  valores lidos, baseando-se na utilização do conceito de propriedade distributiva. Dica: se forem lidas as variáveis  A, B, C e D, devem ser somados e multiplicados os valores de A com B, A com C e A com D; depois B com C, B  com D e por último C com D. Note que para cada operação serão utilizadas seis combinações. Assim sendo,  devem ser realizadas doze operações de processamento, sendo seis para as adições e seis para as  multiplicações. 
+ '''
+cont= 0
+soma = []
+multi = []
+
+números = [int(input('número A: ')),int(input('número B: ')) ,int(input('número C: '))
+,int(input('número D: ')) ]
+
+for i in range (0,3):
+    for c in range(0, 4):
+        if c <= i:
+            continue
+        adi = números[i] + números[c]
+        mut = números[i] * números[c]
+        soma.append(adi)
+        multi.append(mut)
+print('-=' * 30)
+print(f'{'ADIÇÃO':>30}')
+for i in range(0, 3):
+    for c in range(0,4):
+        if c <= i:
+            continue
+        else:
+            print(f'[{números[i]} + {números[c]} = {soma[cont]}]', end=' ')
+            cont+= 1
+    print()
+
+cont = 0
+print('-='*30)
+print(f'{'MULTIPLICAÇÃO':>35}')
+for i in range (0,3):
+    for c in range(0,4):
+        if c <= i:
+            continue
+        else:
+            print(f'[{números[i]} * {números[c]} = {multi[cont]}]', end=' ')
+            cont += 1
+    print()
