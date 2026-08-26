807783a4a5903a0c0179e73aa00486484ca89f5b
DymasKawam <dymaskauam@gmail.com>
2026-08-26 07:24:39 -0300

    Exercício fernando

--- exercicios65/exercício11.py
+++ exercicios65/exercício11.py
@@ -1,11 +1,10 @@
 '''11) Ler dois valores inteiros para as variáveis A e B, efetuar a troca dos valores de modo que a variável A passe a
 possuir o valor da variável B, e a variável B passe a possuir o valor da variável A. Apresentar os valores trocados.
 '''
-
-num1 = int(input('Valor A: '))
-num2 = int(input('Valor B: '))
+numeros = (int(input('Digite um número: ')), int(input('Digite outro número: ')))
 
 print(f'A:{num1} , B:{num2}')
+
 
 
 print(f'A:{num1} , B:{num2}')
