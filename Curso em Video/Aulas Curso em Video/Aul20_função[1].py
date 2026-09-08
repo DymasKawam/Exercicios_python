87a58268dfa8added0d8f16e1121d53b1c3068d1
DymasKawam <dymaskauam@gmail.com>
2026-09-08 11:52:14 -0300

    Aula21

--- Curso em Video/Aulas Curso em Video/Aul20_função[1].py
+++ Curso em Video/Aulas Curso em Video/Aul20_função[1].py
@@ -1,3 +1,5 @@
+#Cria a função "MostrarLinha"
+'''
 def MostraLinha():
     print('-' *60)
 
@@ -13,3 +15,29 @@ MostraLinha()
 MostraLinha()
 print(f'{'Hello':^50}')
 MostraLinha()
+'''
+'''
+def tit(txt):
+    print('=-' * 30)
+    print(f'-- {txt} --')
+    print('-=' * 30)
+
+tit(str(input('Digite algo para texto:')))
+tit
+'''
+'''
+def soma(a, b):
+    print(f'A: {a} e B: {b}')
+    res = a + b
+    print(res)
+
+
+soma(b= 3, a= 9)
+'''
+def contador(* num):
+    for valor in num:
+        print(f'[{valor}]', end=' ')
+
+contador(9,5,3,2,6)
+contador(0,3)
+contador(8,8,7)
